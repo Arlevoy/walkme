59f8f69e99bbbb2071c386aa5f63f43e7d43f0ed
Arthur Levoyer <arthurl@bam.tech>
2022-09-23 21:08:42 +0200

    :wrench: (Eslint) Configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,15 @@
 module.exports = {
-  plugins: ['react', 'react-native'],
+  plugins: ['react-native', 'react-hooks', 'eslint-plugin-import', '@typescript-eslint'],
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:react-native/all', // Enables all rules from react-native
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ], // Specifies the ESLint parser
   parserOptions: {
+    sourceType: 'module', // Allows for the use of imports
     ecmaFeatures: {
       jsx: true,
     },
@@ -10,6 +18,11 @@ module.exports = {
     'react-native/react-native': true,
   },
   rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '_', ignoreRestSiblings: true },
+    ],
     'react-native/no-unused-styles': 2,
     'react-native/split-platform-components': 2,
     'react-native/no-inline-styles': 2,
